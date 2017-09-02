92228094bada7ec8a0bb4548a9f1bbca99426d95
sarra khazri <khazri.sarah@gmail.com>
2017-09-02 02:45:36 -0400

    add test

---
+++ test/test-events.js
@@ -0,0 +1,48 @@
+process.env.NODE_ENV = 'test';
+const mongoose = require('mongoose'),
+  event = require('../app/models/event'),
+  server = require('../server'),
+  chai = require('chai'),
+  should = require('chai').should(),
+  expect = require('chai').expect(),
+  chaiHttp = require('chai-http');
+
+  chai.use(chaiHttp);
+
+  describe('/GET EVENTS', () => {
+    beforeEach((done) => {
+     event.remove({},(error) => {
+       done();
+     });
+    });
+   /*
+  * Test the /GET route
+  */
+   describe('/GET Events', () => {
+    it('it should get all events', (done) => {
+      chai.request(server)
+      .get('/events')
+      .end((err, res) => {
+         res.should.have.status(200);
+         done();
+      });
+     });
+   });
+
+   describe('/POST event', () => {
+    it('it should  POST a event', (done) => {
+      var event = {
+       name: "Weightlifting",
+       description: "Lifting heavy things up"
+      }
+      chai.request(server)
+          .post('/events/create')
+          .send(event)
+          .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+            done();
+          });
+    });
+   });
+  });
