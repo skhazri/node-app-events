4eba44ff3832651366c7f1e023750955c6ba61ab
sarra khazri <khazri.sarah@gmail.com>
2017-09-02 02:40:48 -0400

    add config files

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ require('dotenv').config();
 // grab our dependencies
 const express    = require('express'),
   app            = express(),
-  port           = process.env.PORT || 8081,
+  port           = process.env.PORT || 3000,
   expressLayouts = require('express-ejs-layouts'),
   mongoose       = require('mongoose'),
   bodyParser     = require('body-parser'),
   session        = require('express-session'),
   cookieParser   = require('cookie-parser'),
   flash          = require('connect-flash'),
-  expressValidator = require('express-validator');
+  expressValidator = require('express-validator'),
+  morgan = require('morgan'),
+  config = require('config');
 
 // configure our application ===================
 // set sessions and cookie parser
@@ -30,9 +32,15 @@ app.use(express.static(__dirname + '/public'));
 // set ejs as our templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-
 // connect to our database
-mongoose.connect(process.env.DB_URI);
+// mongoose.connect(process.env.DB_URI);
+mongoose.connect(config.DB_URI);
+
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
+}
 
 // use body parser to grab info from a form
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -45,3 +53,5 @@ app.use(require('./app/routes'));
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
+
+module.exports = app;
